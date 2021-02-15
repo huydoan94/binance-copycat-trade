d045879b05b840b8fbe2b1fd5374c4261141e939
Huy <huydoan94.dev@gmail.com>
2021-02-15 18:43:06 +0700

    Minor improve

--- src/binance-symbol.js
+++ src/binance-symbol.js
@@ -4,19 +4,21 @@ import { memoize } from 'lodash';
 export class BinanceSymbol {
   symbols = []
   fetchSymbolsTimeout = null
+  showConsole = true
 
   fetchSymbols = async () => {
     clearTimeout(this.fetchSymbolsTimeout);
 
-    console.log('Get symbols');
+    if (this.showConsole) console.log('Get symbols');
     try {
       const { data = {} } = await axios.get('/exchangeInfo');
       this.symbols = (data.symbols || []);
-      console.log(`Symbol def count: ${this.symbols.length}`);
+      if (this.showConsole) console.log(`Symbol def count: ${this.symbols.length}`);
     } catch (e) {
       console.error(`Symbols get fail: ${JSON.stringify(e.response.data)}`);
     }
 
+    this.showConsole = false;
     this.fetchSymbolsTimeout = setTimeout(this.fetchSymbols, 30 * 60 * 1000);
   }
 
