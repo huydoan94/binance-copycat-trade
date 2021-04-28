2c788555c66823e3f33e2baee0e5313a83e6ae7d
Henry <huy.doandang@linecorp.com>
2021-04-28 16:11:12 +0700

    Fix start script

--- scripts/start.js
+++ scripts/start.js
@@ -7,6 +7,11 @@ process.env.NODE_ENV = get(process.env, 'NODE_ENV', 'development');
 require('./compile').then(({ path: buildPath, filename }) => {
   console.log(chalk.cyan('Starting App'));
 
+  const stopHandler = () => process.exit(100);
+  process.on('SIGTERM', stopHandler);
+  process.on('SIGINT', stopHandler);
+  process.on('SIGHUP', stopHandler);
+
   let firstStart = true;
   nodemon({
     script: path.join(buildPath, filename),
